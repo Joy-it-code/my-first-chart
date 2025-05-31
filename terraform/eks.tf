c3d3f97031c650264f857c41a3615dde7a2f29e2
Joy-it-code <joynwat2014@gmail.com>
2025-05-31 03:19:12 +0100

    update file

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -27,3 +27,38 @@ module "eks" {
     Environment = "dev"
   }
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  #load_config_file       = false
+}
+ 
+ 
+# resource "kubernetes_config_map" "aws_auth" {
+#   metadata {
+#     name      = "aws-auth"
+#     namespace = "kube-system"
+#   }
+ 
+#   data = {
+#     mapRoles = yamlencode([
+#       {
+#         rolearn  = "arn:aws:iam::586794450782:role/jenkins-eks-ecr-role"
+#         username = "jenkins"
+#         groups   = ["system:masters"] 
+#       }
+#     ])
+#   }
+ 
+#   depends_on = [module.eks]
+# }
